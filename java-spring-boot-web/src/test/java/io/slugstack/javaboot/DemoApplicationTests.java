f8bb94e0160436048bb9e3b2b8af98c18e24aa83
Aaron Gershman <5619476+aegershman@users.noreply.github.com>
2020-10-24 17:47:11 -0500

    spring boot web bumps (#16)
    * spring boot demo basic
    
    * spring boot 2.4 milestone

---
+++ java-spring-boot-web/src/test/java/io/slugstack/javaboot/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.slugstack.javaboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class DemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
