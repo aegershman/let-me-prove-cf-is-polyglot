c42f9ed5b4ecfa21528ad7530e01e7b7c432367e
Aaron Gershman <aegershman@gmail.com>
2020-11-03 12:21:51 -0600

    formatting of main, formatting of reqs

--- python-web/main.py
+++ python-web/main.py
@@ -5,9 +5,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def root():
-    return 'I am instance ' + str(os.getenv("CF_INSTANCE_INDEX", 0))
+    return 'I am instance ' + str(os.getenv('CF_INSTANCE_INDEX', 0))
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 8080.
-    port = int(os.getenv("PORT", 8080))
+    port = int(os.getenv('PORT', 8080))
     app.run(host='0.0.0.0', port=port)
