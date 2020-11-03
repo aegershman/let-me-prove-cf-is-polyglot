d274e8c126fbb3888a6efa3d6785d087f2dbabfb
Aaron Gershman <aegershman@gmail.com>
2020-11-03 11:17:24 -0600

    super minor efficiency improvement to main

--- python-web/main.py
+++ python-web/main.py
@@ -3,11 +3,11 @@ import os
 
 app = Flask(__name__)
 
-port = int(os.getenv("PORT", 8080))
-
 @app.route('/')
 def root():
     return 'I am instance ' + str(os.getenv("CF_INSTANCE_INDEX", 0))
 
 if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 8080.
+    port = int(os.getenv("PORT", 8080))
     app.run(host='0.0.0.0', port=port)
